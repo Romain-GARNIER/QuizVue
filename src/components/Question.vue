57598c8caadddcfe7b1ec3fd064457627888cb08
romain <garnierr10@gmail.com>
2024-09-22 14:40:35 +0200

    Add timer before change question

--- src/components/Question.vue
+++ src/components/Question.vue
@@ -9,12 +9,12 @@
          :correctAnswer="question.correct_answer"
         />
       </label>
-    <button :disabled="!hasAnswer" @click="Submit()">Question suivante</button>
+    <!--button :disabled="!hasAnswer" @click="Submit()">Question suivante</button-->
   </div>
 </template>
 
 <script setup>
-import { ref, computed } from 'vue'
+import { ref, computed, watch, onUnmounted, onMounted } from 'vue'
 import { shuffleArray } from '../functions/array'
 import Choice from './Choice.vue'
 
@@ -27,6 +27,25 @@ const hasAnswer = computed(() => selectedResponse.value !== null)
 const randomChoices = computed(() => shuffleArray(props.question.choices))
 
 const emits = defineEmits(['submit'])
+
+watch(selectedResponse, () => {
+  setTimeout(() => {
+    emits('submit', selectedResponse.value)
+  }, 2000)
+})
+
+let timer
+
+onMounted(() => {
+  timer = setTimeout(() => {
+    selectedResponse.value = ''
+  }, 5000)
+})
+
+onUnmounted(() => {
+  clearTimeout(timer)
+})
+
 
 function Submit() {
   emits('submit', selectedResponse.value)
